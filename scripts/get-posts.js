2e2a131f11b5d85a49c3ddbdf2fcb436cff6ef5f
Ian King <ian.antking@gmail.com>
2020-02-22 23:41:21 +0000

    add date and content to blog posts

--- scripts/get-posts.js
+++ scripts/get-posts.js
@@ -1,3 +1,22 @@
 fetch('content/posts.json')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    const blogPosts = document.getElementById('posts')
+    console.log(blogPosts);
+    data.forEach(postData => {
+      const postCard = document.createElement('div');
+      const date = document.createElement('h3');
+      const title = document.createElement('h3');
+      const content = document.createElement('div');
+
+      date.innerHTML = postData.date;
+      title.innerHTML = postData.title;
+      content.innerHTML = postData.content;
+
+      postCard.appendChild(title);
+      postCard.appendChild(date);
+      postCard.appendChild(content);
+
+      blogPosts.appendChild(postCard);
+    });
+  })
