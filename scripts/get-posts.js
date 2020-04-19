a2e22f6155d66ab7aa660a57a842c19bd5d59174
Justin Sandahl <sandahl.j.d@gmail.com>
2020-04-19 11:55:36 +0100

    first change, added another section in the json named intro, which then gets created as its own div section above the content with a className of intro, added css code to highlight section in red

--- scripts/get-posts.js
+++ scripts/get-posts.js
@@ -6,16 +6,20 @@ fetch('content/posts.json')
       const postCard = document.createElement('div');
       const date = document.createElement('h3');
       const title = document.createElement('h3');
+      const intro = document.createElement('div');
       const content = document.createElement('div');
 
       date.innerHTML = postData.date;
       title.innerHTML = postData.title;
       content.innerHTML = postData.content;
+      intro.innerHTML = postData.intro;
+      intro.classList.add('intro');
 
       postCard.appendChild(title);
       postCard.appendChild(date);
+      postCard.appendChild(intro);
       postCard.appendChild(content);
-
+      
       blogPosts.appendChild(postCard);
     });
   })
